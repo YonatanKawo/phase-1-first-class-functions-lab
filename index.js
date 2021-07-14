ebaceae770700aab7b69f5e4952adec30138bcc7
Yonatan Kawo <yonas478@gmail.com>
2021-07-13 20:32:39 -0500

    commit tree e5a54d9b91d8171db5d914b9db86a3bdc0690c5e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier = function(fares){
+    return function(value){
+        return value*fares;
+    };
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(arrayOfDrivers,returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
